e95ccde8f62f82fb309d729d411c22fd0202a039
William Hernandez <whs465@gmail.com>
2025-08-07 21:29:01 -0500

    cambio iconos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,38 @@
 // src/app/layout.tsx
 import './globals.css'
 import { ReactNode } from 'react'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Visor NACHAM',
   description: 'Web app para ver el contenido de archivos NACHAM',
-    icons: {
-    icon: '/favicon.png',        
-    apple: '/favicon.png',       
-    shortcut: '/favicon.png'     
+  icons: {
+    icon: '/favicon.png',
+    apple: '/favicon.png',
+    shortcut: '/favicon.png'
   }
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es">
+      <Script
+        id="gtm-head"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','G-DJC85LJVNM');
+          `,
+        }}
+      />
       <body className="bg-gray-100 text-gray-800 overflow-y-scroll">
         <div className="w-full max-w-[1070px] mx-auto min-h-screen flex flex-col">
           {/* Contenido principal */}
